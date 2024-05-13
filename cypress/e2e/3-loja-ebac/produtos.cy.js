4504296d52e35fea9607afe0385a0bc18be05d74
Esperty <gabrielstraube@hotmail.com>
2024-05-13 07:35:56 -0300

    Criando massa de dados

--- cypress/e2e/3-loja-ebac/produtos.cy.js
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -25,10 +25,24 @@ describe('Funcionalidade: Produtos', () => {
         cy.get('.product_title').should('contain', 'Zeppelin Yoga Pant');
     });
 
-    it.only('Deve adicionar produto ao carrinho', () => {
+    it('Deve adicionar produto ao carrinho', () => {
+        let qtd = 7
         produtosPage.buscarProduto('Abominable Hoodie')
-        produtosPage.addProdutoCarrinho()
+        produtosPage.addProdutoCarrinho('M', 'Green', qtd)
 
-        cy.get('.woocommerce-message').should('contain', '“Abominable Hoodie” foi adicionado no seu carrinho.')
+        cy.get('.woocommerce-message').should('contain', qtd + ' × “Abominable Hoodie” foram adicionados no seu carrinho.')
+    });
+
+    it.only('Deve adicionar produto ao carrinho buscando da massa de dados', () => {
+        cy.fixture('produtos').then(dados =>{
+        produtosPage.buscarProduto(dados[0].nomeProduto)
+        produtosPage.addProdutoCarrinho(
+            dados[0].tamanho, 
+            dados[0].cor, 
+            dados[0].quantidade)
+        cy.get('.woocommerce-message').should('contain', dados[0].nomeProduto)
+        })
+
+        
     });
 });
